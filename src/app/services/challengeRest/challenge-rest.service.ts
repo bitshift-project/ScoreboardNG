446d1ae8b4029afa8e36432e3678b01465773709
Arendt, Kilian(SE-5/7SIN2) <kilian.arendt@volkswagen.de>
2025-02-06 11:52:09 +0100

    fix challenge rest service

--- src/app/services/challengeRest/challenge-rest.service.ts
+++ src/app/services/challengeRest/challenge-rest.service.ts
@@ -1,11 +1,14 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environment/environment';
 import { Challenge, ChallengeType } from '../../domain/Challenge';
+import { TagsRestService } from '../tagsRest/tags-rest.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ChallengeRestService {
+  tagsRestService: TagsRestService = inject(TagsRestService);
+
   constructor() {}
 
   createChallenge(
@@ -32,24 +35,46 @@ export class ChallengeRestService {
     });
   }
 
-  async getAllChallengesForProject(projectId : number): Promise<Challenge[]>{
+  async getAllChallengesForProject(projectId: number): Promise<Challenge[]> {
     const response = await fetch(
       `${environment.apiUrl}/project/${projectId}/challenge`
     );
-    return response.json().then((data) => data as Challenge[]);
+    return response.json().then((data) => this.convertMultipleChallenges(data));
   }
 
-  async getChalllengeForId(challengeId: number): Promise<Challenge>{
+  async getChalllengeForId(challengeId: number): Promise<Challenge> {
     const response = await fetch(
       `${environment.apiUrl}/project/challenge/${challengeId}`
     );
-    return response.json().then((data) => data as Challenge);
+    return response.json().then((data) => this.convertToChallenge(data));
   }
 
-  deleteChallenge(challengeId: number): Promise<Response>{
+  deleteChallenge(challengeId: number): Promise<Response> {
     return fetch(`${environment.apiUrl}/project/challenge/${challengeId}`, {
       method: 'DELETE',
     });
   }
 
+  private async convertToChallenge(challengeData: any): Promise<Challenge> {
+    return {
+      name: challengeData.name,
+      shortDescription: challengeData.shortDescription,
+      longDescription: challengeData.longDescription,
+      points: challengeData.points,
+      challengeType: challengeData.challengeType,
+      projectId: challengeData.projectId,
+      challengeId: challengeData.challengeId,
+      tags: await this.tagsRestService.getTagsForChallenge(
+        challengeData.challengeId
+      ),
+    };
+  }
+
+  private async convertMultipleChallenges(data: any): Promise<Challenge[]> {
+    const promiseList: Promise<Challenge>[] = [];
+    for (const element of data) {
+      promiseList.push(this.convertToChallenge(element));
+    }
+    return Promise.all(promiseList);
+  }
 }
