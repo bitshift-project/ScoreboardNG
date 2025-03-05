e82b33e1a17a9865e8f693d344f2b67f4fbfba65
straczi <k.arendt0302@gmail.com>
2025-03-05 14:55:54 +0100

    add styling for pinned cards

--- src/app/components/challenge-card/challenge-card.component.ts
+++ src/app/components/challenge-card/challenge-card.component.ts
@@ -4,15 +4,17 @@ import { Challenge, Tag } from '../../domain/Challenge';
 import { TagChipComponent } from '../tag-chip/tag-chip.component';
 import { CommonModule } from '@angular/common';
 import { ShareDataService } from '../../services/shareData/share-data.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-challenge-card',
-  imports: [MatCardModule, TagChipComponent, CommonModule],
+  imports: [MatCardModule, TagChipComponent, CommonModule, MatIconModule],
   templateUrl: './challenge-card.component.html',
   styleUrl: './challenge-card.component.scss',
 })
 export class ChallengeCardComponent {
   challenge = input.required<Challenge>();
+  isPinned = input<boolean>(false);
   sortedTags: Tag[] = [];
   shareDataService = inject(ShareDataService);
   isCompleted = false;
